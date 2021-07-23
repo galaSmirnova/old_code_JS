d6016e2fc13d67182553c7ef7c17458e1cbd68bf
Galina Smirnova <gal4enok13@gmail.com>
2021-07-23 12:44:24 +0300

    Les 55

---
+++ Lesson_JS_55/script/script_55_3.js
@@ -0,0 +1,5 @@
+let arr = [2, 5, 6, 7, 11];
+for (let i = 0; i < arr.length; i++) {
+    arr[i] += 10;
+}
+console.log(arr);
---
+++ Lesson_JS_56/script/script_56_1.js
@@ -0,0 +1,12 @@
+let arr = [];
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+arr.push(5);
+arr.push(6);
+arr.push(7);
+arr.push(8);
+arr.push(9);
+arr.push(10);
+console.log(arr);
